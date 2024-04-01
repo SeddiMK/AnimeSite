25cd17f8080790169d6ec9f0c054d52884531c1e
Max <web.egorovm@gmail.com>
2024-04-01 11:53:58 +0300

    not_

--- src/app/App.js
+++ src/app/App.js
@@ -6,7 +6,7 @@ import Footer from '../components/footer/Footer';
 
 export default function App() {
   return (
-    <div className="сontainer">
+    <div className="container">
       <Header />
       <Main />
       <Footer />
