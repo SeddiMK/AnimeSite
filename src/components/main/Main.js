c16e1caf2cda5b7198651a1159b2fb6f15d86984
Max <web.egorovm@gmail.com>
2024-04-08 13:56:56 +0300

    scss-main

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -56,22 +56,6 @@ const Main = () => {
                 </div>
               </div>
 
-              <div className="media__title">Здесь заголовок</div>
-              <div className="media__text">
-                Давно выяснено, что при оценке дизайна и композиции читаемый
-                текст мешает сосредоточиться. Lorem Ipsum используют потому, что
-                тот обеспечивает более или менее стандартное заполнение шаблона,
-                а также реальное распределение букв и пробелов в абзацах,
-                которое не получается при простой дубликации "Здесь ваш текст..
-                Здесь ваш текст.. Здесь ваш текст.." Многие программы
-                электронной вёрстки и редакторы HTML используют Lorem Ipsum в
-                качестве текста по умолчанию, так что поиск по ключевым словам
-                "lorem ipsum" сразу показывает, как много веб-страниц всё ещё
-                дожидаются своего настоящего рождения. За прошедшие годы текст
-                Lorem Ipsum получил много версий. Некоторые версии появились по
-                ошибке, некоторые - намеренно (например, юмористические
-                варианты).
-              </div>
               <dl className="media__desc desc-media">
                 <dt className="desc-media__dt">Тип</dt>
                 <dd className="desc-media__dd">Фильм</dd>
@@ -88,15 +72,40 @@ const Main = () => {
             </div>
           </div>
           <div className="content__media description">
-            <div className="description__text"></div>
+            <div className="description__title">Здесь заголовок</div>
+            <div className="description__text">
+              Давно выяснено, что при оценке дизайна и композиции читаемый текст
+              мешает сосредоточиться. Lorem Ipsum используют потому, что тот
+              обеспечивает более или менее стандартное заполнение шаблона, а
+              также реальное распределение букв и пробелов в абзацах, которое не
+              получается при простой дубликации "Здесь ваш текст.. Здесь ваш
+              текст.. Здесь ваш текст.." Многие программы электронной вёрстки и
+              редакторы HTML используют Lorem Ipsum в качестве текста по
+              умолчанию, так что поиск по ключевым словам "lorem ipsum" сразу
+              показывает, как много веб-страниц всё ещё дожидаются своего
+              настоящего рождения. За прошедшие годы текст Lorem Ipsum получил
+              много версий. Некоторые версии появились по ошибке, некоторые -
+              намеренно (например, юмористические варианты).
+            </div>
           </div>
-        </div>
-        <div className="main__coment">
-          <button className="main__btn-send-comment" onClick={openForm}>
-            Send comment
-          </button>
+          <div className="content__player">
+            <iframe
+              width="1520"
+              height="579"
+              src="https://www.youtube.com/embed/ErgOZ5mZYho"
+              title="Поднятие уровня в одиночку | Официальный трейлер"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen></iframe>
+          </div>
+          <div className="content__coment">
+            <button className="content__btn-send-comment" onClick={openForm}>
+              Send comment
+            </button>
 
-          <FormMain />
+            <FormMain />
+          </div>
         </div>
       </div>
     </main>
