59cc1169fd046e29a2b7edc5b5147229e4131a95
Max <web.egorovm@gmail.com>
2024-04-18 13:43:28 +0300

    login_scss

--- src/components/inputForm/loginForm.jsx
+++ src/components/inputForm/loginForm.jsx
@@ -1,10 +1,40 @@
 import React, { useState } from 'react';
 import './loginForm.scss';
 import { Form, Link } from 'react-router-dom';
+import {
+  animated,
+  useSpring,
+  config,
+  useSpringRef,
+  useChain,
+} from 'react-spring';
 
 const LoginForm = () => {
   const [valInpLog, setValInpLog] = useState('');
   const [valInpPass, setValInpPass] = useState('');
+
+  // custom checkbox animated
+  const [isChecked, setIsChecked] = useState(false);
+  const checkboxAnimationRef = useSpringRef();
+  const checkmarkAnimationRef = useSpringRef();
+  const checkboxAnimationStyle = useSpring({
+    backgroundColor: isChecked ? 'var(--btn-send-com)' : '#fff',
+    borderColor: isChecked ? 'var(--btn-send-com)' : '#ddd',
+    config: config.gentle,
+    ref: checkboxAnimationRef,
+  });
+  const [checkmarkLength, setCheckmarkLength] = useState(null);
+  const checkmarkAnimationStyle = useSpring({
+    x: isChecked ? 0 : checkmarkLength,
+    config: config.gentle,
+    ref: checkmarkAnimationRef,
+  });
+  useChain(
+    isChecked
+      ? [checkboxAnimationRef, checkmarkAnimationRef]
+      : [checkmarkAnimationRef, checkboxAnimationRef],
+    [0, 0.1] // -> delay by 0.1 seconds
+  );
 
   const changeInpValLogin = (e) => {
     // console.log(e.current, 'e.current');
@@ -42,15 +72,51 @@ const LoginForm = () => {
         />
       </div>
       <div className="remember-checkbox">
-        <label className="custom-control custom-checkbox" htmlFor="remember_me">
+        <label
+          id="custom-checkbox-login"
+          className="custom-control custom-checkbox"
+          htmlFor="remember_me">
           <input
-            className="custom-control-input mr-1"
+            className="custom-control-input"
             type="checkbox"
             id="remember_me"
             name="_remember_me"
-            value="on"
+            onChange={() => {
+              setIsChecked(!isChecked);
+            }}
           />
-          <span className="custom-control-indicator"></span>
+          {/* <span
+            // className="custom-control-indicator"
+            className={`custom-control-indicator checkbox ${
+              isChecked ? 'checkbox--active' : ''
+            }`}
+            // we hide it for screen readers
+            aria-hidden="true"></span> */}
+
+          <animated.svg
+            style={checkboxAnimationStyle}
+            className={`custom-control-indicator checkbox ${
+              isChecked ? 'checkbox--active' : ''
+            }`}
+            // This element is purely decorative so
+            // we hide it for screen readers
+            aria-hidden="true"
+            viewBox="0 0 15 11"
+            fill="none">
+            <animated.path
+              d="M1 4.5L5 9L14 1"
+              strokeWidth="2"
+              // stroke={isChecked ? '#fff' : 'none'} // only show the checkmark when `isCheck` is `true`
+              ref={(ref) => {
+                if (ref) {
+                  setCheckmarkLength(ref.getTotalLength());
+                }
+              }}
+              stroke="#fff"
+              strokeDasharray={checkmarkLength}
+              strokeDashoffset={checkmarkAnimationStyle.x}
+            />
+          </animated.svg>
           <span className="custom-control-description">Запомнить меня</span>
         </label>
       </div>
@@ -59,7 +125,7 @@ const LoginForm = () => {
           <div className="form__btn btn-inp">
             <button
               type="submit"
-              className="btn btn-lg btn-block btn-primary"
+              className="btn btn-lg"
               id="_submit"
               name="_submit"
               value="Войти">
