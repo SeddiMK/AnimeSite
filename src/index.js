1dc45fdd53057f7387c499276afe6766f4bc631e
Max <web.egorovm@gmail.com>
2024-04-24 12:05:25 +0300

    user_registration

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals.js';
 
 import App from './app/App.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
