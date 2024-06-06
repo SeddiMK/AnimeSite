95fa9efe1f4c03e36816beadea91dd544f409d76
Max <web.egorovm@gmail.com>
2024-06-06 18:17:42 +0300

    dpl-fix

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -16,6 +16,7 @@ import Footer from '../components/footer/Footer';
 
 // pages
 import Main from '../pages/main/Main';
+import NewList from '../pages/newList/NewList';
 import FullDescItem from '../pages/fullDescItem/FullDescItem';
 import RandomAnime from '../pages/randomAnime/RandomAnime';
 import SearchHeader from '../components/searchHeader/SearchHeader';
@@ -24,7 +25,6 @@ import Login from '../pages/login/Login';
 import Registration from '../pages/regisration/Registration';
 import LoginUserCabinet from '../pages/loginUserCabinet/LoginUserCabinet';
 import Error from '../pages/error/Error';
-import NewList from '../pages/newList/NewList';
 
 // router
 import { router } from '../router/router';
