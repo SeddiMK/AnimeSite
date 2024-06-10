47b4cb76410aa8c589d411655e9bff4f9c1ae90c
Max <web.egorovm@gmail.com>
2024-06-10 21:44:42 +0300

    useAuth

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -22,9 +22,9 @@ const Header: React.FC = () => {
   // const { isAuth, id } = useAuth();
   const [toggleRandom, setToggleRandom] = useState(true);
 
-  // useEffect(() => {
-  //   dispatch(clickRandomHeder(toggleRandom));
-  // }, [toggleRandom]);
+  useEffect(() => {
+    dispatch(clickRandomHeder(toggleRandom));
+  }, [toggleRandom]);
 
   // const { email, token, id } = useSelector(
   //   (state: RootState) => state.userSlice
@@ -32,67 +32,64 @@ const Header: React.FC = () => {
   // const [burgerClick, setBurgerClick] = useState<boolean>(true);
 
   return (
-    <>
-      <div>,h,,,nm,n,njkghfdghjkljhg</div>
-    </>
-    // <header className="header">
-    //   {/* <ErrorBoundary fallback={<p>Something went wrong</p>}>  */}
-    //   {/* </ErrorBoundary>  */}
-    //   <div className="header__container">
-    //     <Link to="/" className="header__logo">
-    //       ME
-    //       {/* <img src="#" alt="Image logo" />   */}
-    //     </Link>
-    //     <nav className="header__nav menu">
-    //       <ul className="nav-mobile">
-    //         <NavMobile />
-    //       </ul>
-    //       <ul className="menu__list active">
-    //         <MenuList />
-    //       </ul>
-    //       <ul className="menu__list-r">
-    //         <li className="menu__list-r search-header">
-    //           <SearchHeader />
-    //         </li>
-    //         <li
-    //           className="menu__list-r login-btn"
-    //           onClick={() => {
-    //             if (isAuth) {
-    //               dispatch(removeUser());
-    //               navigate('/');
-    //             }
-    //           }}>
-    //           <Link
-    //             to={`/login`}
-    //             preventScrollReset={true}
-    //             className="menu__link"
-    //             onClick={() =>
-    //               localStorage.setItem(
-    //                 'remeberMe',
-    //                 JSON.stringify(Boolean(false))
-    //               )
-    //             }
-    //             id="navbar-login">
-    //             <span className="material-symbols-outlined">
-    //               {isAuth ? 'logout' : 'login'}
-    //             </span>
-    //             <span>{isAuth ? 'Выйти' : 'Войти'}</span>
-    //           </Link>
-    //         </li>
-    //         {isAuth && (
-    //           <li className="menu__list-r log-user-cab">
-    //             <Link
-    //               to={`login/user/id:${id}`}
-    //               className="menu__link"
-    //               id="cabinet">
-    //               Кабинет
-    //             </Link>
-    //           </li>
-    //         )}
-    //       </ul>
-    //     </nav>
-    //   </div>
-    // </header>
+    <header className="header">
+      {/* <ErrorBoundary fallback={<p>Something went wrong</p>}>  */}
+      {/* </ErrorBoundary>  */}
+      <div className="header__container">
+        <Link to="/" className="header__logo">
+          ME
+          {/* <img src="#" alt="Image logo" />   */}
+        </Link>
+        <nav className="header__nav menu">
+          <ul className="nav-mobile">
+            <NavMobile />
+          </ul>
+          <ul className="menu__list active">
+            <MenuList />
+          </ul>
+          <ul className="menu__list-r">
+            <li className="menu__list-r search-header">
+              <SearchHeader />
+            </li>
+            <li
+              className="menu__list-r login-btn"
+              onClick={() => {
+                // if (isAuth) {
+                //   dispatch(removeUser());
+                //   navigate('/');
+                // }
+              }}>
+              <Link
+                to={`/login`}
+                preventScrollReset={true}
+                className="menu__link"
+                onClick={() =>
+                  localStorage.setItem(
+                    'remeberMe',
+                    JSON.stringify(Boolean(false))
+                  )
+                }
+                id="navbar-login">
+                {/* <span className="material-symbols-outlined">
+                  {isAuth ? 'logout' : 'login'}
+                </span>
+                <span>{isAuth ? 'Выйти' : 'Войти'}</span> */}
+              </Link>
+            </li>
+            {/* {isAuth && (
+              <li className="menu__list-r log-user-cab">
+                <Link
+                  to={`login/user/id:${id}`}
+                  className="menu__link"
+                  id="cabinet">
+                  Кабинет
+                </Link>
+              </li>
+            )} */}
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 };
 
