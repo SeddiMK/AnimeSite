666132d6b266f9294f84de765d89cca1dee712d2
Max <web.egorovm@gmail.com>
2024-06-11 17:43:55 +0300

    firebase-Ok_variab-in-vercel

--- src/firebase.ts
+++ src/firebase.ts
@@ -17,6 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth();
+// const auth = getAuth();
 
-export default auth;
+// export default auth;
