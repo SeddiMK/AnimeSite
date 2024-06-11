48eaaf6a61a6e5267351c34ae6c58a167cf8a613
Max <web.egorovm@gmail.com>
2024-06-11 14:46:36 +0300

    a

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -13,38 +13,38 @@ export const useAuth = () => {
 
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  // const auth = getAuth(); // !!!!!!!!!!!--------------------------------------------------
-  // auth.useDeviceLanguage(); // определение языка девайса ---------------------------
+  const auth = getAuth(); // !!!!!!!!!!!--------------------------------------------------
+  auth.useDeviceLanguage(); // определение языка девайса ---------------------------
   // const user: User | null = auth.currentUser;
   // // остаться в сисеме без повторной аутентификации remeberMe
-  // useEffect(() => {
-  //   //------------------------------------------------------------
-  //   const checkBoxRememberMe = localStorage.getItem('remeberMe');
+  useEffect(() => {
+    //------------------------------------------------------------
+    const checkBoxRememberMe = localStorage.getItem('remeberMe');
 
-  //   if (checkBoxRememberMe === 'true')
-  //     onAuthStateChanged(auth, (user) => {
-  //       if (user) {
-  //         // console.log(user);
+    if (checkBoxRememberMe === 'true')
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // console.log(user);
 
-  //         // User is signed in, see docs for a list of available properties
-  //         // https://firebase.google.com/docs/reference/js/auth.user
-  //         // photoUrl: user.photoURL,
-  //         dispatch(
-  //           setUser({
-  //             email: user.email,
-  //             token: user.refreshToken,
-  //             id: user.uid,
-  //             displayName: user.displayName,
-  //             photoUrl: user.photoURL,
-  //           })
-  //         );
-  //       } else {
-  //         // User is signed out
-  //         // ...
-  //         // checkbox remember me
-  //       }
-  //     });
-  // }, [auth, dispatch]);
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/auth.user
+          // photoUrl: user.photoURL,
+          dispatch(
+            setUser({
+              email: user.email,
+              token: user.refreshToken,
+              id: user.uid,
+              displayName: user.displayName,
+              photoUrl: user.photoURL,
+            })
+          );
+        } else {
+          // User is signed out
+          // ...
+          // checkbox remember me
+        }
+      });
+  }, [auth, dispatch]);
 
   // photoUrl,
   return {
