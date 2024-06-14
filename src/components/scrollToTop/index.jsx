2475a586bf22a0b26339112c4606ef89b160282c
Max <web.egorovm@gmail.com>
2024-06-14 18:09:00 +0300

    fix_scroolToTop

--- src/components/scrollToTop/index.jsx
+++ src/components/scrollToTop/index.jsx
@@ -2,19 +2,13 @@ import { useEffect, useLayoutEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
-  // const { pathname } = useLocation();
   console.log('ScrollToTop----');
-
-  // useEffect(() => {
-  //   window.scrollTo(0, 0);
-  // }, [pathname]);
 
   const location = useLocation(); //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   // Scroll to top if path changes
   useLayoutEffect(() => {
-    // document.body.scrollTo({ top: 0, behavior: "smooth" });
     window.scrollTo(0, 0);
-    // document.documentElement.scrollTo(0, 0);
+    document.getElementById('root')?.scrollTo(0, 0);
   }, [location.pathname]);
 
   return null;
