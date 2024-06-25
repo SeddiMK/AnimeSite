547535246174edfe7f3a6d4b708b4bf10892d66c
SeddiMK <web.egorovm@gmail.com>
2024-06-25 20:14:14 +0300

    getElementById_fix-memory

--- src/components/scrollToTop/index.jsx
+++ src/components/scrollToTop/index.jsx
@@ -5,8 +5,26 @@ const ScrollToTop = () => {
 	const location = useLocation()
 	// Scroll to top if path changes
 	useLayoutEffect(() => {
-		window.scrollTo(0, 0)
-		// document.getElementById('root')?.scrollTo(0, 0);
+		// window.scroll({
+		// 	top: 0,
+		// 	left: 0,
+		// 	behavior: 'smooth',
+		// })
+		// document.getElementById('root').scrollTo(0, 0)
+
+		console.log(window.scrollY, 'window.scrollY')
+		if (window.scrollY > 400) {
+		}
+		// document.getElementById('root').scroll({
+		// 	top: 0,
+		// 	left: 0,
+		// 	behavior: 'smooth',
+		// })
+		document.querySelector('#root').scroll({
+			top: 0,
+			left: 0,
+			behavior: 'smooth',
+		})
 	}, [location.pathname])
 
 	return null
